be6a0ecf7a4f24362133cda0d5dadbfc5bf46270
reycs <dqkhanh.18it1@sict.udn.vn>
2020-06-27 21:25:37 +0700

    update

--- public/caller.js
+++ public/caller.js
@@ -7,7 +7,7 @@ const configuration = {
     },
     iceServers: [{ urls: 'stun:stun.l.google.com:19302' }, {
         'urls': 'turn:numb.viagenie.ca',
-        'credential': 'khanh1920',
+        'credential': 'n@wSFmUAgXFUx8V',
         'username': 'reycs2000@gmail.com'
     }]
 }
